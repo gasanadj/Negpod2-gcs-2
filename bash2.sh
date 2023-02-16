5db9636ded4d3283811d920cebbcb86d658adcbe
mulimuoki001 <b.muoki@alustudent.com>
2023-02-16 22:48:08 +0200

    Was a real test

--- bash2.sh
+++ bash2.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-#print random numbers and their squares
-import random
-num = random.randint(0, 100000)
-sr= num**(1/2)
-echo("The first random number generated is {}".format(num))
-echo("The square root is ", "%.0f" %sr)
-
-number = random.randint(0, 10000)
-square_root = number**(0.5)
-echo("The second random number generated is {}".format(number))
-echo("The square root is ", "%.0f" %square_root)
-
-sum = int(sr) + int(square_root)
-echo("The sum of their square roots is {}".format(sum))
+#print random numbers and their square roots
+first=$RANDOM
+second=$RANDOM
+square_one=$(echo "$first" | awk ' {printf "%.0f", sqrt($1)}')
+square_two=$(echo "$second" | awk ' {printf "%.0f", sqrt($1)}')
+sum=$(($square_one + $square_two))
+echo "The first random number generated is $first"
+echo "The square root is $square_one"
+echo "The second random number generated is $second"
+echo "The square root is $square_two"
+echo "The sum of their square roots is $sum"
