fff79e8e0e606fc57bbc472e26771d727d2698a1
BRIAN MULI MUOKI <116681226+mulimuoki001@users.noreply.github.com>
2023-02-16 17:12:00 +0200

    Update bash2.sh

--- bash2.sh
+++ bash2.sh
@@ -3,13 +3,13 @@
 import random
 num = random.randint(0, 100000)
 sr= num**(1/2)
-echo("The first random number generated is {}".format(num))
-echo("The square root is ", "%.0f" %sr)
+print("The first random number generated is {}".format(num))
+print("The square root is ", "%.0f" %sr)
 
 number = random.randint(0, 10000)
 square_root = number**(0.5)
-echo("The second random number generated is {}".format(number))
-echo("The square root is ", "%.0f" %square_root)
+print("The second random number generated is {}".format(number))
+print("The square root is ", "%.0f" %square_root)
 
 sum = int(sr) + int(square_root)
-echo("The sum of their square roots is {}".format(sum))
+print("The sum of their square roots is {}".format(sum))
