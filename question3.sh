1813e78364c72ad1e53f104a32a6e7a776c22983
Mgeni-E <e.mgeni@alustudent>
2023-02-17 21:20:29 +0200

    Sdfddfds

---
+++ question3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Printing the number of words and number of whitespaces
+
+count=0
+echo "Enter a phrase"
+read x
+for i in $x 
+do
+((count++))
+space=$(expr length "$x" - length `echo "$x" | sed "s/ //g"`)
+done
+echo "The number of words is $count"
+echo "The number of whitespaces is $space"
